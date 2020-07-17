9f64f3bbc3a0ee419ec808b439070389563dac8b
doakcovington <doakcovington@protonmail.com>
2020-07-16 20:47:19 -0700

    google oauth

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express'); //grants access to express library
+const passport = require(`passport`);
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/greeting', (req,res) => { //get route handler
-    res.send({hi: 'there'}); 
-})
+passport.use(new GoogleStrategy()); //creates a new instance of GoogleStrategy auth
 
 const PORT = process.env.PORT || 5000; //dynamic port for heroku
 app.listen(PORT); 
