8a7723c5c78c29b0e09b192dd34ea4a23077e9fe
doakcovington <doakcovington@protonmail.com>
2020-07-17 19:35:24 -0700

    Refracotr

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express'); //grants access to express library
-const passport = require(`passport`);
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-passport.use(new GoogleStrategy()); //creates a new instance of GoogleStrategy auth
+require('./routes/authRoutes')(app); 
+
 
 const PORT = process.env.PORT || 5000; //dynamic port for heroku
 app.listen(PORT); 
